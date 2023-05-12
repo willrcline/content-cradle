862c7c21ab13ee683ea45ebd70b18801064b9589
willcline613 <willrcline.atx@gmail.com>
2023-05-11 22:23:26 -0500

    file structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000, function () {
+    console.log('Express/Handlebars server started on port 3000');
+});
