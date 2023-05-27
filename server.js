0f3d38d238bf6269b3f36de0dab49f84f6590a6f
willcline613 <willrcline.atx@gmail.com>
2023-05-27 12:33:07 -0500

    if JAWSDB_URL env var exists, connect using that. Added max age to the cookie

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const hbs = exphbs.create();
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 3600000
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
